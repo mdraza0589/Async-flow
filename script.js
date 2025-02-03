e441d845d9b3a001cd2cb9a12f4836e177cb8b8d
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-02-03 21:17:22 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+        console.log('first');
+        console.log('Second');
+
+        setTimeout(() => {
+            console.log('third');
+        }, 2000);
+
+        console.log('Four');
+        console.log('fifth');
+        
